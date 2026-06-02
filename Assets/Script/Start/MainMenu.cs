d245b309468e63d70ffb66b68e87f1209afa9cf9
TheOwlgugu <1046940332@qq.com>
2026-06-02 21:19:25 +0800

    添加开始菜单

---
+++ Assets/Script/Start/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // 开始游戏：加载游戏场景
+    public void StartGame()
+    {
+        // "GameScene" 需要替换成你实际游戏场景的名称
+        SceneManager.LoadScene("GameScenes");
+    }
+
+    // 退出游戏
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        // 在 Unity 编辑器中停止运行
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            // 在打包后的游戏中退出程序
+            Application.Quit();
+#endif
+    }
+}
